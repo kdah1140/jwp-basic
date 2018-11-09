9a65d04df97c48a064eb351fca6037e6f723de99
3hagnyeon <3hagnyeon@192.168.0.122>
2018-11-09 09:18:34 +0900

    로그인 로그아웃 구현 완료

---
+++ src/main/java/next/web/LoginServlet.java
@@ -0,0 +1,52 @@
+package next.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import core.db.DataBase;
+import next.model.User;
+
+@WebServlet("/user/login")
+public class LoginServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String userId = req.getParameter("userId");
+		String password = req.getParameter("password");
+		User user = DataBase.findUserById(userId);
+		if(user == null) {
+			req.setAttribute("loginFailed", true);
+			forword("/user/login_failed.jsp", req, resp);
+			return;
+		}
+		
+		if(user.matchPassword(password)) {
+			HttpSession session = req.getSession();
+			session.setAttribute(UserSessionUtils.USER_SESSION_KEY, user);
+			resp.sendRedirect("/");
+		}else {
+			req.setAttribute("loginFailed", true);
+			forword("/user/login.jsp", req, resp);
+		}
+	}
+	
+	private void forword(String forwordUrl,
+			HttpServletRequest req, 
+			HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher rd = req.getRequestDispatcher("/user/login_failed.jsp");
+		rd.forward(req, resp);
+		
+	}
+	
+	
+
+}
